9057c31d81d6f31d9d60e0095bb31df093b40efa
Milaan Parmar /  Милан пармар / _米兰 帕尔马 <milaanparmar9@hotmail.com>
2021-10-12 18:59:15 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,7 @@
+import sys
+#print(sys.argv[0], argv[1],sys.argv[2])  # this line would print out: filename argument1 argument2
+print('Welcome {}. Enjoy  {} for Data Science ^_^'.format(sys.argv[1], sys.argv[2]))
+
+
+
+
